83f13a575a75ba67911e86122dfe9ac0d6db26fc
Mayuresh Gade <67890675+mayureshgade19@users.noreply.github.com>
2020-07-06 11:42:10 +0530

    Create conditional_statement.py

---
+++ conditional_statement.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+def weird(n):
+    if (n%2 == 0 and 2<=n<=5):
+        print("Not Weird")
+    elif (n%2==0 and n > 20):
+        print("Not Weird")
+    elif(n%2 == 0 and 6<=n<=20):
+        print("Weird")
+    else:
+        print("Weird")
+    
+
+if __name__ == '__main__':
+    N = int(input())
+    weird(N)
